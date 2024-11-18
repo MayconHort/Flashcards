c43b3ff8cfeca0ec948a9eb1bcf3abe6d3c947a7
MayconHort <maycon.hort@escola.pr.gov.br>
2024-11-18 13:37:11 -0300

    class

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
  function  criacartao(categoria,pergunta,resposta){
  let container = document.getElementById('container')
+ let cartao = document.createElement('article')
+ cartao.className = 'cartao'
  }
